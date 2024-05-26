d93ef88b0b8646fdd4a70ca24180392280f8dd72
ptv50157@yahoo.com.tw <ptv50157@yahoo.com.tw>
2024-05-26 22:15:44 +0800

    test

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // create an express app
 const express = require("express")
 const app = express()
-
+const path = require('path');
 // use the express-static middleware
 //app.use(express.static("public"))
-
+app.use(express.static(path.join(__dirname, 'public')));
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+app.get("/test", function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index2.html'));
 })
 
 // start the server listening for requests
